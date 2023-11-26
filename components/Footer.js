ca98ec3a5517bda77c0a56ee35dd8a74e464980e
dragonfly9999 <henwir42@gmail.com>
2023-11-26 17:30:28 +0700

    bt react1

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className='bg-dark text-white p-4'>
+        <p className='m-0 fs-6'>Copyright © Your Website 2023</p>
+      </div>
+    )
+  }
+}
